caa83781149782784ce80b63c30b9ca21ffb810a
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-19 12:52:23 +0300

    requared fields

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 const name = document.getElementById('name');
 const password = document.getElementById('password');
 const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    let messages = []
+    if(name.value === '' || name.value == null) {
+        messages.push('Name is requared')
+    }
+
+    if(messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = message.join(', ')
+    }
 })
+
