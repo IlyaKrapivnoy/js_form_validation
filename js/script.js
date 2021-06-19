291367df0ea3f9dcc052b51ee21aed658dc3cd78
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-19 13:17:02 +0300

    error message fix

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-const name = document.getElementById('name');
-const password = document.getElementById('password');
-const form = document.getElementById('form');
-const errorElement = document.getElementById('error');
+const name = document.getElementById('name')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
 
 form.addEventListener('submit', (e) => {
     let messages = []
-    if(name.value === '' || name.value == null) {
-        messages.push('Name is requared')
+    if (name.value === '' || name.value == null) {
+        messages.push('Name is required')
     }
 
-    if(password.value.length <= 6) {
+    if (password.value.length <= 6) {
         messages.push('Password must be longer than 6 characters')
     }
 
-    if(password.value.length >= 20) {
+    if (password.value.length >= 20) {
         messages.push('Password must be less than 20 characters')
     }
 
-    if(password.value === 'password') {
+    if (password.value === 'password') {
         messages.push('Password cannot be password')
     }
 
-    if(messages.length > 0) {
+    if (messages.length > 0) {
         e.preventDefault()
-        errorElement.innerText = message.join(', ')
+        errorElement.innerText = messages.join(', ')
     }
 })
